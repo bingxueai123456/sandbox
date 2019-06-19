cd37b6fd54d893c351e365325fa56d45d9bad02e
dongbc <dongbc@silvrr.com>
2019-06-19 09:52:53 +0800

    init

---
+++ src/main/java/com/ice/snowflake/TestSpringConfigModule.java
@@ -0,0 +1,44 @@
+package com.ice.snowflake;
+
+import com.alibaba.jvm.sandbox.api.Information;
+import com.alibaba.jvm.sandbox.api.Module;
+import com.alibaba.jvm.sandbox.api.ProcessController;
+import com.alibaba.jvm.sandbox.api.annotation.Command;
+import com.alibaba.jvm.sandbox.api.listener.ext.Advice;
+import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
+import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
+import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+import org.kohsuke.MetaInfServices;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: eclair
+ * @Description:
+ * @Date: 2019/6/19
+ */
+@MetaInfServices(Module.class)
+@Information(id = "my-first-sandbox")
+public class TestSpringConfigModule implements Module {
+	@Resource
+	private ModuleEventWatcher moduleEventWatcher;
+
+	/**
+	 * 改变JsonTestSpringConfigController的testConfig
+	 */
+	@Command("changeTestConfig")
+	public void testChangeTestConfig() {
+		new EventWatchBuilder(moduleEventWatcher)
+				.onClass("com.akulaku.snowflake.controller.outer.JsonTestSpringConfigController")
+				.onBehavior("testConfig")
+				.onWatch(new AdviceListener(){
+					@Override
+					protected void afterReturning(Advice advice) throws Throwable {
+						ProcessController.returnImmediately("I am eclair");
+					}
+				});
+
+
+	}
+
+}
